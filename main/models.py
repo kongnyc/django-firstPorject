7e46599249be1c874cfd8d8453c3d645880e6300
kongnyc <bbsyoow@gmail.com>
2021-10-22 17:14:31 -0400

    updated datetime.now(tz=timezone.utc) to aviod runntime warning DTF received a naive datetime

--- main/models.py
+++ main/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
 from django.db.models.deletion import CASCADE, DO_NOTHING
 
 # Create your models here.
 class Room(models.Model):
     name=models.CharField(max_length=64, unique=True)
     description = models.TextField()
+    date=models.DateField(default=timezone.now)
     password= models.CharField(max_length=31,blank=True)
 
     def __str__(self):
@@ -13,7 +16,7 @@ class Room(models.Model):
     
 class Message(models.Model):
     user=models.ForeignKey(User, null=True, on_delete=DO_NOTHING)
-    time=models.DateField(auto_now_add=True)
+    time=models.DateTimeField(default=datetime.datetime.now(tz=timezone.utc))
     room=models.ForeignKey(Room, on_delete=CASCADE)
     text=models.CharField(max_length=500)
     
