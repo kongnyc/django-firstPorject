052554013e342881137028e4accb36fbc7d2a074
kongnyc <bbsyoow@gmail.com>
2021-11-23 12:09:44 -0500

    add logout user from home/navbar

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     
     path('register/',views.registerUser, name='register'),
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name="logout"),
     
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Room, Message
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from .form import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def registerUser(request):
     form = CreateUserForm()
@@ -14,10 +16,29 @@ def registerUser(request):
     return render(request, 'main/register.html',{'form':form})
 
 def loginPage(request):
-    context={}
-    return render(request, 'main/login.html', context)
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		return render(request, 'main/login.html', {})
+
+def logoutUser(request):
+	logout(request)
+	return redirect('home')
 
 def index(response,roomid):
+    
     room=Room.objects.get(id=roomid)
     return render(response, "main/base.html", {'name':room.name, 'date':room.date, 'description':room.description})
 
